abc186528782cbd8c6ec35a9f6c09757dc8c342f
valentinkelbakh <valentinkelbax@gmail.com>
2023-09-13 04:07:53 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
