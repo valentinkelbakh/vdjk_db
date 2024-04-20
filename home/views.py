0a8815af45ad066b1b622e055ed7a2405948bb07
valentinkelbakh <valentinkelbax@gmail.com>
2024-04-21 01:14:00 +0200

    small fix

--- home/views.py
+++ home/views.py
@@ -40,8 +40,8 @@ class WebhookViewSet(viewsets.ViewSet):
     def create(self, request):
         if request.method == 'POST':
             print(f'Webhook set on:\n{request.data["webhook_url"]}\n')
-            if request.data['content'] == settings.WEBHOOK_PASS:
-                settings.WEBHOOK_ENABLED = True
+            if request.data["webhook_pass"] == settings.WEBHOOK_PASS:
+                settings.webhook_connected = True
                 settings.WEBHOOK_URL = request.data["webhook_url"]
                 webhook = Webhook.objects.get_or_create()[0]
                 webhook.url = request.data["webhook_url"]
