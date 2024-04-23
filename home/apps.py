b89d399f1a341621983448add96e3379b58fdf77
valentinkelbakh <valentinkelbax@gmail.com>
2024-04-24 01:12:17 +0200

    removed webhook-stuff on start of server

--- home/apps.py
+++ home/apps.py
@@ -1,36 +1,6 @@
-import logging
-
-import requests
 from django.apps import AppConfig
-from django.conf import settings
-from django.db.models.signals import post_migrate
-from django.dispatch import receiver
-
-logger = logging.getLogger(__name__)
 
 
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
-
-    def ready(self):
-        if settings.WEBHOOK and not settings.WEBHOOK_CONNECTED:
-            from .models import Webhook
-
-            webhook = Webhook.objects.first()
-            if not webhook:
-                logger.info("No last Webhook URL")
-                return
-            url = webhook.url
-            try:
-                response = requests.get(url)
-                if response.status_code == 200:
-                    settings.WEBHOOK_CONNECTED = True
-                    settings.WEBHOOK_URL = url
-                    logger.info(f"Webhook set on:\n{url}\n")
-                else:
-                    webhook.delete()
-                    logger.info(f"Last Webhook URL is no longer available: {url}")
-            except Exception as e:
-                logger.error(f"Error setting webhook: {e}")
-        return
